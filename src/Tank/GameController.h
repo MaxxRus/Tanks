e86dabd7913145afb06b1071b50384e1ed8a1710
MaxxRus <rus.maxx@gmail.com>
2018-03-23 07:30:06 +0200

    new class diagram

---
+++ src/Tank/GameController.h
@@ -0,0 +1,7 @@
+class GameController
+{
+public:
+	GameController();
+	~GameController();
+};
+
--- src/Tank/main.cpp
+++ src/Tank/main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 
+#include <windows.h>
+
+#include "main.h"
+
+
 using namespace std;
 
 
 int main() 
 {
 	cout << "sdsdsd" << endl;
+	COORD descart;
+	map <COORD, int> myFirstMap;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10 j++)
+		{
+			descart.X = i;
+			descart.Y = j;
+			myFirstMap.insert(pair<COORD, int>(descart, i));
+		}
 
+	}
+
+
+	
+	for (auto it = myFirstMap.begin(); it != myFirstMap.end(); ++it)
+	{
+		cout <<  it->second << endl;
+	}
+
+		
 	system("PAUSE");
 	return 0;
 }
